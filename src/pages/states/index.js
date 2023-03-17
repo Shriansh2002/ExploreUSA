aeeca0a38475e652700acee8819432db97d6f8b3
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-17 08:51:16 +0530

    State Page Header to be also in USA Coloring

--- src/pages/states/index.js
+++ src/pages/states/index.js
@@ -21,8 +21,11 @@ export default function StatesPage() {
             <>
                 <Header />
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
-                    <h1 className="text-4xl text-center font-bold text-gray-800 py-6">
-                        Explore the United States
+                    <h1 className="text-4xl mb-10 text-center font-bold text-gray-800 py-6">
+                        Explore the {" "}
+                        <span className="text-transparent uppercase bg-clip-text bg-gradient-to-r from-red-600 to-blue-600">
+                            United States
+                        </span>
                     </h1>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-between">
                         {states.map((state, _idx) => (
